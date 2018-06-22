115c4805b64afac243b63d5d7747cfec9475c25b
Sumathy Dhakshinamurthy <sumathy.vijay@gmail.com>
2018-06-21 22:13:15 -0500

    Committing the changes

--- src/main/java/org/launchcode/CheeseMvcApplication.java
+++ src/main/java/org/launchcode/CheeseMvcApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CheeseMvcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CheeseMvcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CheeseMvcApplication.class, args);
+    }
 }
